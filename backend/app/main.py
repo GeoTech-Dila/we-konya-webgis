5bbf0a5c17596615607ba617609493f221317723
GeoTech-Dila <ozturkdila670@gmail.com>
2026-05-24 21:54:09 +0300

    try3

--- backend/app/main.py
+++ backend/app/main.py
@@ -21,7 +21,12 @@ app.add_middleware(
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True,
+    pool_size=5,
+    max_overflow=2
+)
 
 
 @app.get("/")
